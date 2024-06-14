386a1b985185d5a9eb904215cc067865b269f9a8
chandanbhagat7s <bhagatchandan287@gmail.com>
2024-06-14 14:07:25 +0530

    made ready for deployment

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const env = require("dotenv");
+const compression = require('compression');
+
+
+
 const globalErrorHandler = require('./utils/globalErrorHandler');
 const userRouter = require('./Routes/userRoutes');
 const adminRouter = require('./Routes/adminRoutes');
 const teacherRoute = require('./Routes/teacherRoutes');
 const appError = require('./utils/appError');
 
+
+
+
 const cloudinary = require('cloudinary');
 const app = express()
 env.config({ path: "./config.env" })
 
+
+
+
 app.use(morgan("dev"))
+app.use(compression())
 
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
@@ -39,7 +50,7 @@ cloudinary.v2.config({
 });
 
 
-
+app.use("/", (req, res) => { res.status(200).send({ status: "working" }) })
 app.use('/api/v1/auth', userRouter)
 app.use("/api/v1/admin", adminRouter)
 app.use("/api/v1/teacher", teacherRoute)
